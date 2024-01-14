104370326e008d9aa2bfdeaa6830a60e0fa075ba
CaleSpen <kesapragadasrivatsav@gmail.com>
2024-01-14 15:20:06 +0530

    dark mode

--- src/components/header.js
+++ src/components/header.js
@@ -5,17 +5,17 @@ import { Navbar, Nav } from 'react-bootstrap';
 const Header = () => {
   return (
     <Navbar bg="light" expand="lg" className="navbar">
-      <Navbar.Brand href="/">Srivatsav Kesapragada</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="ml-auto">
-          <NavLink className="nav-link" to="/">About</NavLink>
-          <NavLink className="nav-link" to="/resume">Resume</NavLink>
-          {/* <NavLink className="nav-link" to="/portfolio">portfolio</NavLink> */}
-          <NavLink className="nav-link" to="/contact">Contact</NavLink>
-          {/* <NavLink className="nav-link" to="/passion">Passion</NavLink> */}
-        </Nav>
-      </Navbar.Collapse>
+        <Navbar.Brand href="/">Srivatsav Kesapragada</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <NavLink className="nav-link" to="/">About</NavLink>
+            <NavLink className="nav-link" to="/resume">Resume</NavLink>
+            {/* <NavLink className="nav-link" to="/portfolio">portfolio</NavLink> */}
+            <NavLink className="nav-link" to="/contact">Contact</NavLink>
+            {/* <NavLink className="nav-link" to="/passion">Passion</NavLink> */}
+          </Nav>
+        </Navbar.Collapse>
     </Navbar>
   );
 }
